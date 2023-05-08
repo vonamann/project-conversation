e877312acfbfcfbf6fc11c863201ef880518f12a
Johann Aman <johann.aman@gmail.com>
2023-05-09 01:59:45 +0200

    Initialize the new project with default status -- “created”

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   has_many :histories, -> { order('created_at DESC') }, class_name: 'ProjectHistory'
   validates :status, inclusion: { in: PROJECT_STATUSES }
+
+  after_initialize { self.status = PROJECT_DEFAULT_STATUS }
 end
--- config/initializers/projects.rb
+++ config/initializers/projects.rb
@@ -1,1 +1,2 @@
 PROJECT_STATUSES = %w[created progress finished].freeze
+PROJECT_DEFAULT_STATUS = PROJECT_STATUSES[0].freeze
