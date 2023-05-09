0ff79293c73610082c958daede806eed041cb15f
Johann Aman <johann.aman@gmail.com>
2023-05-09 02:35:14 +0200

    Project#change_status_to added with some specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,23 @@ class Project < ApplicationRecord
   validates :status, inclusion: { in: PROJECT_STATUSES }
 
   after_initialize { self.status = PROJECT_DEFAULT_STATUS }
+
+  def change_status_to(new_status)
+    Project.transaction do
+      # Do nothing if new status is the same as the old one for simplicity
+      unless status == new_status
+        update(status: new_status)
+        raise ActiveRecord::Rollback unless errors.empty?
+        history = histories.new(status: new_status)
+        unless history.save
+          errors.add(:base, history.errors.full_messages.first)
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+    errors.empty?
+  rescue => e
+    errors.add(:base, "Failed to change status: #{e.message}")
+    false
+  end
 end
---
+++ spec/models/project_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Project do
+  before do
+    @project = Project.create!
+    @original_histories_count = @project.histories.count
+  end
+
+  describe '#change_status_to' do
+    it 'does nothing if same status passed' do
+      @project.change_status_to('created')
+      expect(@project.errors).to be_empty
+      expect(@project.status).to eq('created')
+      expect(@project.histories.count).to eq(@original_histories_count)
+    end
+
+    it 'changes status and creates a new history record' do
+      @project.change_status_to('progress')
+      expect(@project.errors).to be_empty
+      expect(@project.status).to eq('progress')
+      expect(@project.histories.count).to eq(@original_histories_count + 1)
+    end
+
+    it 'rejects incorrect status' do
+      @project.change_status_to('foobar')
+      expect(@project.errors).not_to be_empty
+      expect(@project.histories.count).to eq(@original_histories_count)
+    end
+  end
+end
