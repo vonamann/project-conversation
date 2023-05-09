37f1133e59f8804d3fedcdb90c5bec3c14f24ca1
Johann Aman <johann.aman@gmail.com>
2023-05-09 12:07:15 +0200

    Display last status change (if exists), form for setting new status

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,17 @@
 class ProjectsController < ApplicationController
-  def show
+  before_action do
     @project = Project.singleton
+  end
+
+  def show
+  end
+
+  def update
+    if @project.change_status_to(params[:status])
+      flash.notice = "Status successfully changed to “#{params[:status]}”"
+    else
+      flash.alert = "Failed to change status: #{@project.errors.full_messages.first}"
+    end
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'projects#show'
+  patch '/' => 'projects#update'
   get '/history' => 'project_histories#index'
 end
